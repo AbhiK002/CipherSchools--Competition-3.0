9022f2c514e9af7a4d91bc3144e24b59e06767df
Abhineet Kelley <abhineet.kelley@gmail.com>
2023-10-12 23:56:16 +0530

    Day 3 commit

--- Day 3/Solution3.java
+++ Day 3/Solution3.java
@@ -4,8 +4,51 @@ class Solution3 {
             return;
         }
 
-        Node ptr = head;
+        Node turtle = head;
+        Node rabbit = head;
 
+        // find middle node of linked list using slow and fast pointers
+        while (rabbit != null && rabbit.next != null && rabbit.next.next != null) {
+            turtle = turtle.next;
+            rabbit = rabbit.next.next;
+        }
+
+        // point rabbit to the last node
+        while (rabbit.next != null) rabbit = rabbit.next;
+
+        // reverse the nodes after turtle
+        turtle.next = reverse(turtle.next, rabbit);
+
+        // shift the first node of second half between the nodes of first half repeatedly
+        Node nodeInserter = head;
+
+        while (nodeInserter != turtle) {
+            Node target = turtle.next;
+            turtle.next = turtle.next.next;
+
+            target.next = nodeInserter.next;
+            nodeInserter.next = target;
+
+            nodeInserter = nodeInserter.next.next;
+        }
+    }
+
+    public Node reverse(Node head, Node tail)
+    {
+        if (head == null || head.next == null) return head;
+
+        Node ptr = head;
+        Node last = tail;
+
+        while (head != last)
+        {
+            head = head.next;
+            ptr.next = last.next;
+            last.next = ptr;
+            ptr = head;
+        }
+
+        return head;
     }
 
     public static Node convertToLinkedList(int[] elements) {
@@ -23,6 +66,10 @@ class Solution3 {
     public static void main(String[] args) {
         Node head;
         Solution3 test = new Solution3();
+
+        head = convertToLinkedList(new int[]{1, 2, 3, 4, 5, 6});
+        test.reorderlist(head);
+        System.out.println(head);
 
         head = convertToLinkedList(new int[]{1, 2, 3, 4, 5});
         test.reorderlist(head);
